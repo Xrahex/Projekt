95c718fc7695178c818b34b3641edf088ac8dc21
MagdalenaPlawska <101335062+MagdalenaPlawska@users.noreply.github.com>
2022-05-02 13:26:19 +0200

    cennik,faq,o_nas
    dodanie podstron

--- cennik.php
+++ cennik.php
@@ -6,8 +6,101 @@ $smarty = new Smarty();
 
 $smarty->assign('title','Cennik');
 $smarty->assign('content','
+<main>
 
-<p> Tu będzie cennik </p>
+<div class="container marketing">
+
+ 
+  <div class="row">
+    <div class="col-lg-4">
+      
+    </div><!-- /.col-lg-4 -->
+  </div><!-- /.row -->
+  <hr class="featurette-divider">
+     <h1 class="text-center display-3">Cennik</h1>
+          <p></p>
+		  <table class="table align-middle table table-hover table-bordered">
+  <thead>
+    <tr class="table-dark">
+      
+      <th scope="col">Rodzaj zabiegu</th>
+      <th scope="col">Pierwszy Zabieg</th>
+      <th scope="col">Pakiet 4 zabiegi</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      
+      <td>Blizna do 10 cm</td>
+      <td>760 zł</td>
+      <td>2 280 zł</td>
+    </tr>
+    <tr class="align-bottom">
+      
+      <td>Melasma mała</td>
+      <td>1 000 zł</td>
+      <td>2 400 zł</td>
+    </tr>
+    <tr>
+      
+      <td>Blizny potrądzikowe – twarz</td>
+      <td>2 000 zł</td>
+	  <td>4 000 zł</td>
+    </tr>
+	
+	<tr>
+      
+      <td>Rozstępy – obszar 10 cm x 10 cm</td>
+      <td>880 zł</td>
+	  <td>2 640 zł</td>
+    </tr>
+	
+	
+	<tr>
+      
+      <td>Twarz przebarwienia</td>
+      <td>2 500 zł</td>
+	  <td>6 000 zł</td>
+    </tr>
+	
+	
+	<tr>
+      
+      <td>Tatuaż bardzo mały do 4 cm</td>
+      <td>600 zł</td>
+	  <td>1 680 zł</td>
+    </tr>
+	
+	
+	<tr>
+      
+      <td>Odmładzanie okolic oczu</td>
+      <td>1 100 zł</td>
+	  <td>2 640 zł</td>
+    </tr>
+	
+	
+	<tr>
+      
+      <td>Naczyniak bardzo mały do 4 cm</td>
+      <td>350 zł</td>
+	  <td>1 200 zł</td>
+    </tr>
+	
+	
+	
+	<tr>
+      
+      <td>Blizna oparzeniowa 10 cm x 10 cm</td>
+      <td>880 zł</td>
+	  <td>2 640 zł</td>
+    </tr>
+  </tbody>
+</table>
+          
+        
+
+    </div><!-- /.container -->
 
 
 
--- faq.php
+++ faq.php
@@ -7,9 +7,88 @@ $smarty = new Smarty();
 $smarty->assign('title','FAQ');
 $smarty->assign('content','
 
-<p> Tu będzie FAQ </p>
 
 
+<main>
+
+<div class="container marketing">
+
+ 
+  <div class="row">
+    <div class="col-lg-4">
+      
+    </div><!-- /.col-lg-4 -->
+  </div><!-- /.row -->
+  <hr class="featurette-divider">
+
+     
+          
+		  <!--Section: FAQ-->
+<section>
+  <h1 class="text-center mb-4 pb-2 fw-bold display-3">FAQ</h1>
+  <p class="text-center mb-5">
+    Pytania najczęsciej zadawane przez pacjentów
+  </p>
+
+  <div class="row">
+    <div class="col-md-6 col-lg-4 mb-4">
+      <h6 class="mb-3 text-primary"><i class="far fa-paper-plane text-primary pe-2"></i> Jak przechowywane są dane?</h6>
+      <p>
+        <strong><u>Nie martw się!</u></strong> Wszystkie dane są przechowywane bezpiecznie w serwerze na terytorium 
+		Rzeczpospolitej Polskiej. Dane konsultacji/ wywiadów medycznych są przechowywane w sposób niejawny i 
+		uniemożliwiający bezpośrednią identyfikację. Krytyczne dane użytkownika są przechowywane w postaci zaszyfrowanej.
+      </p>
+    </div>
+
+    <div class="col-md-6 col-lg-4 mb-4">
+      <h6 class="mb-3 text-primary"><i class="fas fa-pen-alt text-primary pe-2"></i> Czy serwis jest bezpieczny? 
+	  Jak przekazywane są dane?</h6>
+      <p>
+        Wszystkie dane są przesyłane szyfrowanym połączeniem SSL.
+      </p>
+    </div>
+
+    <div class="col-md-6 col-lg-4 mb-4">
+      <h6 class="mb-3 text-primary"><i class="fas fa-user text-primary pe-2"></i> Czy serwis jest monitorowany od strony 
+	  technicznej?
+      </h6>
+      <p>
+        Oczywiście, bardzo starannie. Audyty bezpieczeństwa są w Klinika-pixel.pl regułą.
+      </p>
+    </div>
+
+    <div class="col-md-6 col-lg-4 mb-4">
+      <h6 class="mb-3 text-primary"><i class="fas fa-rocket text-primary pe-2"></i> Bardzo dbam o poufność. Kto ma dostęp do 
+	  informacji których udzielam?
+      </h6>
+      <p>
+        Do danych z wywiadu medycznego tylko lekarz. Asystenci uzyskują dostęp do e-maila i numeru telefonu gdy tego wymaga 
+		wyjaśnienie rzeczy nie związanych z medyczną stroną konsultacji. Osoby dbające o techniczną stronę serwisu Klinika-Pixel.pl mają wtórny dostęp do
+		danych i wyłącznie z zakresie wynikającym z przeprowadzenia 
+		działań administracyjnych i utrzymania serwisu (kopie zapasowe, update).
+      </p>
+    </div>
+
+    <div class="col-md-6 col-lg-4 mb-4">
+      <h6 class="mb-3 text-primary"><i class="fas fa-home text-primary pe-2"></i> Kiedy następuje płatność?
+      </h6>
+      Po konsultacji, przed wystawieniem recepty. Jeśli nie chcesz recepty, nie musisz płacić za konsultację.
+       </p>
+    </div>
+
+    <div class="col-md-6 col-lg-4 mb-4">
+      <h6 class="mb-3 text-primary"><i class="fas fa-book-open text-primary pe-2"></i> Nie ma tu odpowiedzi na moje pytanie, do kogo mam napisać?</h6>
+      <p>
+        Wszystkie dane kontaktowe są w zakładce "O nas", jeżeli nie znalazłeś/aś odpowiedzi na swoje pytanie, prosimy o kontakt.
+      </p>
+    </div>
+  </div>
+</section>
+<!--Section: FAQ-->
+          
+        
+
+    </div><!-- /.container -->
 
 
 ');
